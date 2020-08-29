47fd59deb69f1c689ccfba0c0fe33b73b51fb594
long <1663862286@qq.com>
2020-08-29 16:03:13 +0800

    update 0

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
 import React from 'react';
-import {Route,Switch} from 'react-router-dom'
-import Login from './pages/login/login.jsx'
-import Admin from './pages//admin/admin.jsx'
+
 
 function App() {
   return (
     <div className="app">
-      <Switch>
-        <Route path="/login" component={Login}/>
-        <Route path="/admin" component={Admin}/>
-      </Switch>
+
     </div>
   );
 }
